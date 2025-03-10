0ddb9696d6c0be9a470d5912dae0294160ac1b23
unknown <noumanishtiaq53@gmail.com>
2025-03-10 05:10:48 +0500

    frisr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,10 @@
 import { PROJECT_DESCRIPTION, PROJECT_NAME } from "@/configs/env";
+import { AppPropsWithLayout } from "@/interfaces/pages";
 import RootLayout from "@/layouts/root-layout";
 import "@/styles/globals.css";
 import { formatPathname } from "@/utils/project-meta";
-import { NextPage } from "next";
-import type { AppProps } from "next/app";
 import Head from "next/head";
 import { usePathname } from "next/navigation";
-import { ReactElement, ReactNode } from "react";
-
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: any) => page);
